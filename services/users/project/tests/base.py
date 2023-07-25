3448a33bb753e30bf9cc608a9c0bfcc54c00eac9
Blackdot <mbohoumounpouyvanlandry@gmail.com>
2023-07-25 15:05:34 +0100

    update test

--- services/users/project/tests/base.py
+++ services/users/project/tests/base.py
@@ -1,1 +1,18 @@
 
+from flask_testing import TestCase
+from project import app, db 
+
+class BaseTestCase(TestCase):
+    def create_app(self):
+        app.config.from_object('project.config.TestingConfig')
+        return app
+    
+    def setup(self):
+        db.create_all()
+        db.session.commit()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+    
+    
