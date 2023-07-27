e29de54bc94dd681eaa01458b05917685e6c6454
Blackdot <mbohoumounpouyvanlandry@gmail.com>
2023-07-28 00:49:35 +0100

    Update __init__.py

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,40 +1,28 @@
+
 import os
-import datetime
-from flask import Flask, jsonify
+from flask import Flask , jsonify
 from flask_sqlalchemy import SQLAlchemy
 
-# instantiate the app
-
-app = Flask(__name__)
-
-# Set config
-app.config.from_object('project.config.DevelopmentConfig')
-app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object(app_settings)
-
 # instantiate the db
-db = SQLAlchemy(app)
 
-#model
-class User(db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True, autoincrement = True)
-    username = db.Column(db.String(128),nullable=False)
-    email = db.Column(db.String(128),nullable=False)
-    active = db.Column(db.Boolean, default=True , nullable=False)
+db = SQLAlchemy()
 
-    def __init__(self, username , email):
-        self.username = username
-        self.email = email
+def create_app(script_info=None):
 
-#routes
-@app.route('/users/ping', methods=['GET'])
-def ping_pong():
-    return jsonify({
-        'status':'success',
-        'message':'pong!',
-    })
+    # instantiate the app
+    app = Flask(__name__)
+    
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
 
+    # set up extensions
+    db.init_app(app)
 
-if __name__=='__main__':
-    app.run(debug=True)
+    # register blueprints
+    from project.api.users import users_blueprints
+    app.register_blueprint(users_blueprints)
+
+    # shell context for flask cli
+    app.shell_context_processor({'app':app , 'db':db})
+    return app
