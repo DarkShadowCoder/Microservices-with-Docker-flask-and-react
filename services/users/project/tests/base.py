2e77cc83f9a0a6ecd4123070cd3eee21ffb7eca2
Blackdot <mbohoumounpouyvanlandry@gmail.com>
2023-11-15 01:25:10 +0100

    Update base.py

--- services/users/project/tests/base.py
+++ services/users/project/tests/base.py
@@ -1,7 +1,8 @@
 
 from flask_testing import TestCase
-from project import app, db 
+from project import create_app, db 
 
+app=create_app()
 class BaseTestCase(TestCase):
     def create_app(self):
         app.config.from_object('project.config.TestingConfig')
