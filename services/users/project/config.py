76e91e2046d37b46c6e0036d74762855e6feff53
Blackdot <mbohoumounpouyvanlandry@gmail.com>
2023-11-15 01:24:54 +0100

    Update config.py

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -1,24 +1,19 @@
 import os
-import datetime
-from flask import Flask , jsonify
-from flask_sqlalchemy import SQLAlchemy
 
 class BaseConfig:
     """Base configuration"""
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'blackdot'
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class TestingConfig(BaseConfig):
     """Testing configuration"""
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_TEST_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 class ProductionConfig(BaseConfig):
     """Production configuration"""
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_TEST_URL")
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
