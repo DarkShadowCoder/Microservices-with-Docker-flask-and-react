521762e1db920f639aa7a10415f7ae0f5cc8bfef
Blackdot <mbohoumounpouyvanlandry@gmail.com>
2023-11-15 01:24:41 +0100

    Update models.py

--- services/users/project/api/models.py
+++ services/users/project/api/models.py
@@ -6,7 +6,7 @@ class User(db.Model):
     id = db.Column(db.Integer , primary_key = True , autoincrement = True)
     username = db.Column(db.String(128) , nullable=False)
     email = db.Column(db.String(128) , nullable=False)
-    active = db.Collumn(db.Boolean() , default = True , nullable=False)
+    active = db.Column(db.Boolean() , default = True , nullable=False)
 
     def __init__(self , username , email):
         self.username = username
